5b9583559cf8124350118a8830df42a281f9eb1f
uday <udday2014@gmail.com>
2021-11-25 05:15:35 -0500

    Add files via upload

--- nets/operations.py
+++ nets/operations.py
@@ -9,10 +9,79 @@ Created on Tue Nov 16 22:51:21 2021
 import torch
 import torch.nn as nn
 from genotypes import *
+from nets.quant_modules import MixActivConv2d
+import pdb
+
+def channel_shuffle(x, groups):
+    batchsize, num_channels, height, width = x.data.size()
+
+    channels_per_group = num_channels // groups
+    
+    # reshape (n, 32, 224, 224)==>(n, 4, 8, 224, 224)
+    x = x.view(batchsize, groups, 
+        channels_per_group, height, width)
+    
+    # (n, 4, 8, 224, 224) ==> (n, 8, 4, 224, 224)
+    x = torch.transpose(x, 1, 2).contiguous()
+
+    # flatten
+    x = x.view(batchsize, -1, height, width)
+
+    return x
+
+class MixedLayer_PC(nn.Module):
+
+  def __init__(self, C, stride, op_names, conv_func):
+    super(MixedLayer_PC, self).__init__()
+    self.op_names = op_names
+    self.layers = nn.ModuleList()
+    self.mp = nn.MaxPool2d(2,2)
+    self.k = 4
+
+    OPS = {'none': lambda C, stride, affine: Zero(stride),
+           'avg_pool_3x3': lambda C, stride, affine: nn.AvgPool2d(3, stride=stride,
+                                                                  padding=1, count_include_pad=False),
+           'max_pool_3x3': lambda C, stride, affine: nn.MaxPool2d(3, stride=stride, padding=1),
+           'skip_connect': lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, conv_func, affine=affine),
+           'sep_conv_3x3': lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine, conv_func=conv_func),
+           'sep_conv_5x5': lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine, conv_func=conv_func),
+           'sep_conv_7x7': lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine, conv_func=conv_func),
+           'dil_conv_3x3': lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine, conv_func=conv_func),
+           'dil_conv_5x5': lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine,conv_func=conv_func),
+    
+           'conv_7x1_1x7': lambda C, stride, affine: nn.Sequential(
+             nn.ReLU(inplace=False),
+             nn.Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), bias=False),
+             nn.Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), bias=False),
+             nn.BatchNorm2d(C, affine=affine))}
+    
+    for primitive in op_names:
+      layer = OPS[primitive](C //self.k, stride, False)
+      if 'pool' in primitive:
+        layer = nn.Sequential(layer, nn.BatchNorm2d(C //self.k, affine=False))
+      self.layers.append(layer)
+
+
+  def forward(self, x, weights):
+    #channel proportion k=4  
+    dim_2 = x.shape[1]
+    xtemp = x[ : , :  dim_2//self.k, :, :]
+    xtemp2 = x[ : ,  dim_2//self.k:, :, :]
+    temp1 = sum(w * op(xtemp) for w, op in zip(weights, self.layers))
+    #reduction cell needs pooling before concat
+    if temp1.shape[2] == x.shape[2]:
+      ans = torch.cat([temp1,xtemp2],dim=1)
+    else:
+      ans = torch.cat([temp1,self.mp(xtemp2)], dim=1)
+    ans = channel_shuffle(ans,self.k)
+    #ans = torch.cat([ans[ : ,  dim_2//4:, :, :],ans[ : , :  dim_2//4, :, :]],dim=1)
+    #except channe shuffle, channel shift also works
+    return ans
+
 
 
 class MixedLayer(nn.Module):
-  def __init__(self, c, stride, op_names):
+  def __init__(self, c, stride, op_names, conv_func):
     super(MixedLayer, self).__init__()
     self.op_names = op_names
     self.layers = nn.ModuleList()
@@ -28,6 +97,25 @@ class MixedLayer(nn.Module):
                 'dil_conv_5x5'
             ]
     """
+# OPS is a set of layers with same input/output channel.
+
+    OPS = {'none': lambda C, stride, affine: Zero(stride),
+           'avg_pool_3x3': lambda C, stride, affine: nn.AvgPool2d(3, stride=stride,
+                                                                  padding=1, count_include_pad=False),
+           'max_pool_3x3': lambda C, stride, affine: nn.MaxPool2d(3, stride=stride, padding=1),
+           'skip_connect': lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, conv_func, affine=affine),
+           'sep_conv_3x3': lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine, conv_func=conv_func),
+           'sep_conv_5x5': lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine, conv_func=conv_func),
+           'sep_conv_7x7': lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine, conv_func=conv_func),
+           'dil_conv_3x3': lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine, conv_func=conv_func),
+           'dil_conv_5x5': lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine,conv_func=conv_func),
+    
+           'conv_7x1_1x7': lambda C, stride, affine: nn.Sequential(
+             nn.ReLU(inplace=False),
+             nn.Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), bias=False),
+             nn.Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), bias=False),
+             nn.BatchNorm2d(C, affine=affine))}
+    
     for primitive in op_names:
       layer = OPS[primitive](c, stride, False)
       if 'pool' in primitive:
@@ -39,24 +127,7 @@ class MixedLayer(nn.Module):
     return sum([w * layer(x) for w, layer in zip(weights, self.layers)])
 
 
-# OPS is a set of layers with same input/output channel.
 
-OPS = {'none': lambda C, stride, affine: Zero(stride),
-       'avg_pool_3x3': lambda C, stride, affine: nn.AvgPool2d(3, stride=stride,
-                                                              padding=1, count_include_pad=False),
-       'max_pool_3x3': lambda C, stride, affine: nn.MaxPool2d(3, stride=stride, padding=1),
-       'skip_connect': lambda C, stride, affine: Identity() if stride == 1 else FactorizedReduce(C, C, affine=affine),
-       'sep_conv_3x3': lambda C, stride, affine: SepConv(C, C, 3, stride, 1, affine=affine),
-       'sep_conv_5x5': lambda C, stride, affine: SepConv(C, C, 5, stride, 2, affine=affine),
-       'sep_conv_7x7': lambda C, stride, affine: SepConv(C, C, 7, stride, 3, affine=affine),
-       'dil_conv_3x3': lambda C, stride, affine: DilConv(C, C, 3, stride, 2, 2, affine=affine),
-       'dil_conv_5x5': lambda C, stride, affine: DilConv(C, C, 5, stride, 4, 2, affine=affine),
-
-       'conv_7x1_1x7': lambda C, stride, affine: nn.Sequential(
-         nn.ReLU(inplace=False),
-         nn.Conv2d(C, C, (1, 7), stride=(1, stride), padding=(0, 3), bias=False),
-         nn.Conv2d(C, C, (7, 1), stride=(stride, 1), padding=(3, 0), bias=False),
-         nn.BatchNorm2d(C, affine=affine))}
 
 
 class ReLUConvBN(nn.Module):
@@ -64,7 +135,7 @@ class ReLUConvBN(nn.Module):
   Stack of relu-conv-bn
   """
 
-  def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
+  def __init__(self, C_in, C_out, kernel_size, stride, padding, conv_func, affine=True):
     """
     :param C_in:
     :param C_out:
@@ -77,7 +148,7 @@ class ReLUConvBN(nn.Module):
 
     self.op = nn.Sequential(
       nn.ReLU(inplace=False),
-      nn.Conv2d(C_in, C_out, kernel_size, stride=stride, padding=padding, bias=False),
+      nn.Sequential(conv_func(inplane=C_in, outplane=C_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)),
       nn.BatchNorm2d(C_out, affine=affine))
 
   def forward(self, x):
@@ -89,7 +160,7 @@ class DilConv(nn.Module):
   relu-dilated conv-bn
   """
 
-  def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, affine=True):
+  def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, conv_func, affine=True):
     """
     :param C_in:
     :param C_out:
@@ -103,9 +174,13 @@ class DilConv(nn.Module):
 
     self.op = nn.Sequential(
       nn.ReLU(inplace=False),
-      nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding,
-                dilation=dilation, groups=C_in, bias=False),
-      nn.Conv2d(C_in, C_out, kernel_size=1, padding=0, bias=False),
+      nn.Sequential(conv_func(inplane=C_in, outplane=C_in, kernel_size=kernel_size, stride=stride, padding=padding, 
+                     dilation=dilation, groups=C_in, bias=False)),
+      nn.Sequential(MixActivConv2d(inplane=C_in, outplane=C_out, kernel_size=1, padding=0, bias=False)),
+     
+      # nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding,
+      #           dilation=dilation, groups=C_in, bias=False),
+      # nn.Conv2d(C_in, C_out, kernel_size=1, padding=0, bias=False),
       nn.BatchNorm2d(C_out, affine=affine))
 
   def forward(self, x):
@@ -117,7 +192,7 @@ class SepConv(nn.Module):
   implemented separate convolution via pytorch groups parameters
   """
 
-  def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
+  def __init__(self, C_in, C_out, kernel_size, stride, padding, conv_func, affine=True):
     """
     :param C_in:
     :param C_out:
@@ -130,14 +205,23 @@ class SepConv(nn.Module):
 
     self.op = nn.Sequential(
       nn.ReLU(inplace=False),
-      nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding,
-                groups=C_in, bias=False),
-      nn.Conv2d(C_in, C_in, kernel_size=1, padding=0, bias=False),
+      nn.Sequential(conv_func(inplane=C_in, outplane=C_in, kernel_size=kernel_size, stride=stride, padding=padding, 
+                     groups=C_in, bias=False)),
+      nn.Sequential(conv_func(inplane=C_in, outplane=C_in, kernel_size=1, padding=0, bias=False)),
+      # nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=stride, padding=padding,
+      #           groups=C_in, bias=False),
+      # nn.Conv2d(C_in, C_in, kernel_size=1, padding=0, bias=False),
+      
       nn.BatchNorm2d(C_in, affine=affine),
       nn.ReLU(inplace=False),
-      nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=1, padding=padding,
-                groups=C_in, bias=False),
-      nn.Conv2d(C_in, C_out, kernel_size=1, padding=0, bias=False),
+      nn.Sequential(conv_func(inplane=C_in, outplane=C_in, kernel_size=kernel_size, stride=1, padding=padding, 
+                     groups=C_in, bias=False)),
+      nn.Sequential(conv_func(inplane=C_in, outplane=C_out, kernel_size=1, padding=0, bias=False)),
+      
+      # nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=1, padding=padding,
+      #           groups=C_in, bias=False),
+      # nn.Conv2d(C_in, C_out, kernel_size=1, padding=0, bias=False),
+      
       nn.BatchNorm2d(C_out, affine=affine))
 
   def forward(self, x):
@@ -168,13 +252,17 @@ class Zero(nn.Module):
       return x.mul(0.)
     return x[:, :, ::self.stride, ::self.stride].mul(0.)
 
+def conv3x3(conv_func, in_plane, out_plane, kernel_size, stride=1, padding=0, bias=False, **kwargs):
+    "3x3 convolution with padding"
+    return conv_func(in_plane, out_plane, kernel_size=kernel_size, stride=stride,
+                     padding=padding, bias=bias, **kwargs)
 
 class FactorizedReduce(nn.Module):
   """
   reduce feature maps height/width by half while keeping channel same
   """
 
-  def __init__(self, C_in, C_out, affine=True):
+  def __init__(self, C_in, C_out, conv_func, affine=True):
     """
     :param C_in:
     :param C_out:
@@ -185,8 +273,10 @@ class FactorizedReduce(nn.Module):
     assert C_out % 2 == 0
 
     self.relu = nn.ReLU(inplace=False)
-    self.conv_1 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
-    self.conv_2 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
+    self.conv_1 = conv_func(inplane=C_in, outplane=C_out // 2, kernel_size=1, stride=2, padding=0, bias=False)
+    self.conv_2 = conv_func(C_in, C_out // 2, kernel_size=1, stride=2, padding=0, bias=False)
+    # self.conv_1 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
+    # self.conv_2 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
     self.bn = nn.BatchNorm2d(C_out, affine=affine)
 
   def forward(self, x):
@@ -196,7 +286,8 @@ class FactorizedReduce(nn.Module):
     # conv1: [b, c_out//2, d//2, d//2]
     # conv2: []
     # out: torch.Size([32, 32, 16, 16])
-
+    # pdb.set_trace()
+    
     out = torch.cat([self.conv_1(x), self.conv_2(x[:, :, 1:, 1:])], dim=1)
     out = self.bn(out)
     return out
--- nets/search_model.py
+++ nets/search_model.py
@@ -12,13 +12,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from nets.operations import FactorizedReduce, ReLUConvBN, MixedLayer
+from nets.operations import FactorizedReduce, ReLUConvBN, MixedLayer, MixedLayer_PC
+
 from genotypes import PRIMITIVES, Genotype
 
 
 class Cell(nn.Module):
 
-  def __init__(self, num_nodes, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev):
+  def __init__(self, num_nodes, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev, conv_func):
     """
     :param num_nodes: 4, number of layers inside a cell
     :param multiplier: 4
@@ -40,12 +41,12 @@ class Cell(nn.Module):
     if reduction_prev:
       # if prev cell has reduced channel/double width,
       # it will reduce width by half
-      self.preprocess0 = FactorizedReduce(C_prev_prev, C, affine=False)
+      self.preprocess0 = FactorizedReduce(C_prev_prev, C, conv_func, affine=False)
     else:
       self.preprocess0 = ReLUConvBN(C_prev_prev, C, kernel_size=1,
-                                    stride=1, padding=0, affine=False)
+                                    stride=1, padding=0, conv_func=conv_func, affine=False)
     # preprocess1 deal with output from prev cell
-    self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0, affine=False)
+    self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0, conv_func=conv_func, affine=False)
 
     # steps inside a cell
     self.num_nodes = num_nodes  # 4
@@ -59,7 +60,7 @@ class Cell(nn.Module):
       for j in range(2 + i):
         # for reduction cell, it will reduce the heading 2 inputs only
         stride = 2 if reduction and j < 2 else 1
-        layer = MixedLayer(C, stride, op_names=PRIMITIVES)
+        layer = MixedLayer(C, stride, op_names=PRIMITIVES, conv_func=conv_func)
         self.layers.append(layer)
 
   def forward(self, s0, s1, weights):
@@ -91,13 +92,87 @@ class Cell(nn.Module):
     # concat along dim=channel
     return torch.cat(states[-self.multiplier:], dim=1)  # 6 of [40, 16, 32, 32]
 
+class Cell_PC(nn.Module):
+
+  def __init__(self, num_nodes, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev, conv_func):
+    """
+    :param num_nodes: 4, number of layers inside a cell
+    :param multiplier: 4
+    :param C_prev_prev: 48
+    :param C_prev: 48
+    :param C: 16
+    :param reduction: indicates whether to reduce the output maps width
+    :param reduction_prev: when previous cell reduced width, s1_d = s0_d//2
+    in order to keep same shape between s1 and s0, we adopt prep0 layer to
+    reduce the s0 width by half.
+    """
+    super(Cell_PC, self).__init__()
+
+    # indicating current cell is reduction or not
+    self.reduction = reduction
+    self.reduction_prev = reduction_prev
+
+    # preprocess0 deal with output from prev_prev cell
+    if reduction_prev:
+      # if prev cell has reduced channel/double width,
+      # it will reduce width by half
+      self.preprocess0 = FactorizedReduce(C_prev_prev, C, conv_func, affine=False)
+    else:
+      self.preprocess0 = ReLUConvBN(C_prev_prev, C, kernel_size=1,
+                                    stride=1, padding=0, conv_func=conv_func, affine=False)
+    # preprocess1 deal with output from prev cell
+    self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0, conv_func=conv_func, affine=False)
+
+    # steps inside a cell
+    self.num_nodes = num_nodes  # 4
+    self.multiplier = multiplier  # 4
+
+    self.layers = nn.ModuleList()
+
+    for i in range(self.num_nodes):
+      # for each i inside cell, it connects with all previous output
+      # plus previous two cells' output
+      for j in range(2 + i):
+        # for reduction cell, it will reduce the heading 2 inputs only
+        stride = 2 if reduction and j < 2 else 1
+        layer = MixedLayer_PC(C, stride, op_names=PRIMITIVES, conv_func=conv_func)
+        self.layers.append(layer)
+
+  def forward(self, s0, s1, weights, weights2):
+    """
+    :param s0:
+    :param s1:
+    :param weights: [14, 8]
+    :return:
+    """
+    # print('s0:', s0.shape,end='=>')
+    s0 = self.preprocess0(s0)  # [40, 48, 32, 32], [40, 16, 32, 32]
+    # print(s0.shape, self.reduction_prev)
+    # print('s1:', s1.shape,end='=>')
+    s1 = self.preprocess1(s1)  # [40, 48, 32, 32], [40, 16, 32, 32]
+    # print(s1.shape)
+
+    states = [s0, s1]
+    offset = 0
+    # for each node, receive input from all previous intermediate nodes and s0, s1
+    for i in range(self.num_nodes):  # 4
+      # [40, 16, 32, 32]
+      # import pdb; pdb.set_trace()
+      s = sum(weights2[offset+j]*self.layers[offset+j](h, weights[offset+j]) for j, h in enumerate(states))
+      offset += len(states)
+      # append one state since s is the elem-wise addition of all output
+      states.append(s)
+      # print('node:',i, s.shape, self.reduction)
+
+    # concat along dim=channel
+    return torch.cat(states[-self.multiplier:], dim=1)  # 6 of [40, 16, 32, 32]
 
 class Network(nn.Module):
   """
   stack number:layer of cells and then flatten to fed a linear layer
   """
 
-  def __init__(self, C, num_cells,
+  def __init__(self, C, num_cells, conv_func, 
                num_nodes=4, multiplier=4, stem_multiplier=3, num_classes=10, img_channel=3):
     """
     :param C: 16
@@ -114,6 +189,7 @@ class Network(nn.Module):
     self.num_cells = num_cells
     self.num_nodes = num_nodes
     self.multiplier = multiplier
+    self.conv_func = conv_func
 
     # stem_multiplier is for stem network,
     # and multiplier is for general cell
@@ -139,7 +215,7 @@ class Network(nn.Module):
 
       # [cp, h, h] => [multiplier*c_curr, h/h//2, h/h//2]
       # the output channels = multiplier * c_curr
-      cell = Cell(num_nodes, multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev)
+      cell = Cell(num_nodes, multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev, self.conv_func, pc=False)
       # update reduction_prev
       reduction_prev = reduction
 
@@ -237,6 +313,214 @@ class Network(nn.Module):
                         reduce=gene_reduce, reduce_concat=concat)
 
     return genotype
+
+  def complexity_loss(self):
+        size_product = []
+        loss = 0
+        for m in self.modules():
+            if isinstance(m, self.conv_func):
+                loss += m.complexity_loss()
+                size_product += [m.size_product]
+        normalizer = size_product[0].item()
+        loss /= normalizer
+        return loss
+
+class Network_PC(nn.Module):
+  """
+  stack number:layer of cells and then flatten to fed a linear layer
+  """
+
+  def __init__(self, C, num_cells, conv_func, 
+               num_nodes=4, multiplier=4, stem_multiplier=3, num_classes=10, img_channel=3):
+    """
+    :param C: 16
+    :param num_cells: number of cells of current network
+    :param num_nodes: nodes num inside cell
+    :param multiplier: output channel of cell = multiplier * ch
+    :param stem_multiplier: output channel of stem net = stem_multiplier * ch
+    :param num_classes: 10
+    """
+    super(Network_PC, self).__init__()
+
+    self.C = C
+    self.num_classes = num_classes
+    self.num_cells = num_cells
+    self.num_nodes = num_nodes
+    self.multiplier = multiplier
+    self.conv_func = conv_func
+
+    # stem_multiplier is for stem network,
+    # and multiplier is for general cell
+    C_curr = stem_multiplier * C  # 3*16
+    # stem network, convert 3 channel to c_curr
+    self.stem = nn.Sequential(  # 3 => 48
+      nn.Conv2d(img_channel, C_curr, 3, padding=1, bias=False),
+      nn.BatchNorm2d(C_curr))
+
+    # c_curr means a factor of the output channels of current cell
+    # output channels = multiplier * c_curr
+    C_prev_prev, C_prev, C_curr = C_curr, C_curr, C  # 48, 48, 16
+    self.cells = nn.ModuleList()
+    reduction_prev = False
+    for i in range(num_cells):
+
+      # for layer in the middle [1/3, 2/3], reduce via stride=2
+      if i in [num_cells // 3, 2 * num_cells // 3]:
+        C_curr *= 2
+        reduction = True
+      else:
+        reduction = False
+
+      # [cp, h, h] => [multiplier*c_curr, h/h//2, h/h//2]
+      # the output channels = multiplier * c_curr
+      # import pdb; pdb.set_trace()
+      cell = Cell_PC(num_nodes, multiplier, C_prev_prev, C_prev, C_curr, reduction, reduction_prev, self.conv_func)
+      # update reduction_prev
+      reduction_prev = reduction
+
+      self.cells += [cell]
+
+      C_prev_prev, C_prev = C_prev, multiplier * C_curr
+
+    # adaptive pooling output size to 1x1
+    self.global_pooling = nn.AdaptiveAvgPool2d(1)
+    # since cp records last cell's output channels
+    # it indicates the input channel number
+    self.classifier = nn.Linear(C_prev, num_classes)
+
+    # k is the total number of edges inside single cell, 14
+    k = sum(1 for i in range(self.num_nodes) for j in range(2 + i))
+    num_ops = len(PRIMITIVES)  # 8
+
+    self.alpha_normal = nn.Parameter(torch.randn(k, num_ops))
+    self.alpha_reduce = nn.Parameter(torch.randn(k, num_ops))
+    self.beta_normal = nn.Parameter(torch.randn(k))
+    self.beta_reduce = nn.Parameter(torch.randn(k))
+    
+    with torch.no_grad():
+      # initialize to smaller value
+      self.alpha_normal.mul_(1e-3)
+      self.alpha_reduce.mul_(1e-3)
+      self.beta_normal.mul_(1e-3)
+      self.beta_reduce.mul_(1e-3)
+      
+    self._arch_parameters = [self.alpha_normal, self.alpha_reduce, self.beta_normal, self.beta_reduce]
+
+  def arch_parameters(self):
+    return self._arch_parameters
+
+  def forward(self, x):
+    """
+    in: torch.Size([3, 3, 32, 32])
+    stem: torch.Size([3, 48, 32, 32])
+    cell: 0 torch.Size([3, 64, 32, 32]) False
+    cell: 1 torch.Size([3, 64, 32, 32]) False
+    cell: 2 torch.Size([3, 128, 16, 16]) True
+    cell: 3 torch.Size([3, 128, 16, 16]) False
+    cell: 4 torch.Size([3, 128, 16, 16]) False
+    cell: 5 torch.Size([3, 256, 8, 8]) True
+    cell: 6 torch.Size([3, 256, 8, 8]) False
+    cell: 7 torch.Size([3, 256, 8, 8]) False
+    pool:   torch.Size([16, 256, 1, 1])
+    linear: [b, 10]
+    :param x:
+    :return:
+    """
+    # print('in:', x.shape)
+    # s0 & s1 means the last cells' output
+    s0 = s1 = self.stem(x)  # [b, 3, 32, 32] => [b, 48, 32, 32]
+    # print('stem:', s0.shape)
+          
+    for i, cell in enumerate(self.cells):
+      # architecture weights are shared across all reduction cell or normal cell
+      # according to current cell's type, it choose which architecture parameters
+      # to use
+      if cell.reduction:  # if current cell is reduction cell
+        weights = F.softmax(self.alpha_reduce, dim=-1)
+        n = 3
+        start = 2
+        weights2 = F.softmax(self.beta_reduce[0:2], dim=-1)
+        for i in range(self.num_nodes-1):
+          end = start + n
+          tw2 = F.softmax(self.beta_reduce[start:end], dim=-1)
+          start = end
+          n += 1
+          weights2 = torch.cat([weights2,tw2],dim=0)
+      else:
+        weights = F.softmax(self.alpha_normal, dim=-1)  # [14, 8]
+        n = 3
+        start = 2
+        weights2 = F.softmax(self.beta_normal[0:2], dim=-1)
+        for i in range(self.num_nodes-1):
+          end = start + n
+          tw2 = F.softmax(self.beta_normal[start:end], dim=-1)
+          start = end
+          n += 1
+          weights2 = torch.cat([weights2,tw2],dim=0)
+      # execute cell() firstly and then assign s0=s1, s1=result
+      # import pdb; pdb.set_trace()
+      s0, s1 = s1, cell(s0, s1, weights, weights2)  # [40, 64, 32, 32]
+      # print('cell:',i, s1.shape, cell.reduction, cell.reduction_prev)
+      # print('\n')
+
+    # s1 is the last cell's output
+    out = self.global_pooling(s1)
+    # print('pool', out.shape)
+    logits = self.classifier(out.view(out.size(0), -1))
+
+    return logits
+
+  def genotype(self):
+
+    def _parse(weights,weights2):
+      gene = []
+      n = 2
+      start = 0
+      for i in range(self.num_nodes):
+        end = start + n
+        W = weights[start:end].copy()
+        W2 = weights2[start:end].copy()
+        for j in range(n):
+          W[j,:]=W[j,:]*W2[j]
+        edges = sorted(range(i + 2), key=lambda x: -max(W[x][k] for k in range(len(W[x])) if k != PRIMITIVES.index('none')))[:2]
+        ops = np.argmax(W[edges, 1:], axis=1) + 1
+        gene += [(PRIMITIVES[op], edge) for op, edge in zip(ops, edges)]
+        start = end
+        n += 1
+      return gene
+  
+    n = 3
+    start = 2
+    weightsr2 = F.softmax(self.beta_reduce[0:2], dim=-1)
+    weightsn2 = F.softmax(self.beta_normal[0:2], dim=-1)
+    for i in range(self._steps-1):
+      end = start + n
+      tw2 = F.softmax(self.beta_reduce[start:end], dim=-1)
+      tn2 = F.softmax(self.beta_normal[start:end], dim=-1)
+      start = end
+      n += 1
+      weightsr2 = torch.cat([weightsr2,tw2],dim=0)
+      weightsn2 = torch.cat([weightsn2,tn2],dim=0)
+    gene_normal = _parse(F.softmax(self.alphas_normal, dim=-1).data.cpu().numpy(),weightsn2.data.cpu().numpy())
+    gene_reduce = _parse(F.softmax(self.alphas_reduce, dim=-1).data.cpu().numpy(),weightsr2.data.cpu().numpy())
+
+    concat = range(2+self.num_nodes-self._multiplier, self.num_nodes+2)
+    genotype = Genotype(
+      normal=gene_normal, normal_concat=concat,
+      reduce=gene_reduce, reduce_concat=concat
+    )
+    return genotype
+
+  def complexity_loss(self):
+        size_product = []
+        loss = 0
+        for m in self.modules():
+            if isinstance(m, self.conv_func):
+                loss += m.complexity_loss()
+                size_product += [m.size_product]
+        normalizer = size_product[0].item()
+        loss /= normalizer
+        return loss
 
 
 # if __name__ == '__main__':
